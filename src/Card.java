fb642f3aa8a439cca54efdafdac14f9d85c78ce4
cosyntxd <110003521+cosyntxd@users.noreply.github.com>
2023-12-06 22:18:15 -0800

    hopefully this is not going to break everything; I have no idea how to play poker

--- src/Card.java
+++ src/Card.java
@@ -1,12 +1,20 @@
+import processing.core.PImage;
+import processing.core.PApplet;
+import processing.core.PImage;
+
 public class Card {
     private int faceValue;
     private int suite;
+
+    private PImage image;
 
     public Card(int faceValue, int suite){
         this.faceValue = faceValue;
         this.suite = suite;
     }
-
+    public String getImagePath() {
+        return "assets/card/card" + suite + "-" + faceValue + ".png";
+    }
     public int getFaceValue(){
         return faceValue;
     }
--- src/Game.java
+++ src/Game.java
@@ -5,16 +5,20 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Game extends PApplet {
-    ArrayList<Card> deck;
-    PImage background;
+    private ArrayList<Card> deck;
+    private ArrayList<Card> table;
+    private ArrayList<Player> players;
+    private int playerTurn = 0;
+    private boolean switchScreen = false;
+    private int pot = 0;
+    private PImage background;
 
     public void settings() {
         size(1000, 492);
-
     }
 
     public void setup() {
-        background = loadImage("APCS.PokerTable.jpeg");
+        background = loadImage("assets/APCS.PokerTable.jpeg");
         deck = new ArrayList<>();
     }
 
@@ -23,32 +27,38 @@ public class Game extends PApplet {
      * tick each object (have it update itself), and draw each object
      */
     public void draw() {
-        image(background, 0, 0);
-    }
+        if (switchScreen) {
+            fill(255, 255, 255);
+            text("Player " + playerTurn + "'s turn is over. Press p to continue to your turn", 380, 240);
+        } else {
+            image(background, 0, 0);
 
-    public void initializeDeck(){
-        for (int value = 1; value <= 13; value++) {
-            for (int suite = 1; suite <= 4; suite++) {
-                Card card = new Card(value, suite);
-                deck.add(card);
+        }
+    }
+    public void keyPressed() {
+        if ((key == 'p' || key == 'P') && switchScreen) {
+            switchScreen = false;
+            playerTurn = (playerTurn + 1) % players.size();
+        }
+    }
+    public void initializeDeck(int amount){
+        // Common in casinos to use multiple decks together to prevent card counting
+        for (int i = 0; i < amount; i++) {
+            for (int value = 1; value <= 13; value++) {
+                for (int suite = 1; suite <= 4; suite++) {
+                    deck.add(new Card(value, suite));
+                }
             }
         }
-
         Collections.shuffle(deck);
     }
 
     public void initializeBoard(){
-        initializeDeck();
+        initializeDeck(3);
         Player P1 = new Player(1000, true);
         Player P2 = new Player(1000, false);
     }
 
-    public void playerSwitch(int playerNum){
-        fill(0, 0 , 0);
-        rect(0, 0, 1000, 492);
-        fill(255, 255, 255);
-        text("Player " + playerNum + "'s turn is over. Press p to continue to your turn", 400, 240);
-    }
 
     public static void main(String[] args) {
         PApplet.main("Game");
